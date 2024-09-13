e1e9646e5c0ddddc18a9b1a49b181d90618d8b74
joshiishaan007 <joshiishaan007@gmail.com>
2024-09-13 23:10:14 +0530

    Sign_In and Sign_Up page done

---
+++ app/src/main/java/com/example/instagram/RegisterActivity.java
@@ -0,0 +1,129 @@
+package com.example.instagram;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText fullname,email,password,username;
+    private Button register;
+    private TextView text_login;
+
+    FirebaseAuth auth;
+    DatabaseReference reference;
+    ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register);
+
+        fullname = findViewById(R.id.fullname);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        username = findViewById(R.id.username);
+        register = findViewById(R.id.register);
+        text_login = findViewById(R.id.text_login);
+
+        auth = FirebaseAuth.getInstance();
+
+        text_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressDialog = new ProgressDialog(RegisterActivity.this);
+                progressDialog.setMessage("Please wait...");
+                progressDialog.show();
+
+                String str_username = username.getText().toString();
+                String str_fullname = fullname.getText().toString();
+                String str_password = password.getText().toString();
+                String str_email = email.getText().toString();
+
+                if(TextUtils.isEmpty(str_username)){
+                    Toast.makeText(RegisterActivity.this, "Please enter Username", Toast.LENGTH_SHORT).show();
+                } else if(TextUtils.isEmpty(str_fullname)){
+                    Toast.makeText(RegisterActivity.this, "Please enter Fullname", Toast.LENGTH_SHORT).show();
+                } else if(TextUtils.isEmpty(str_email)){
+                    Toast.makeText(RegisterActivity.this, "Please enter Email", Toast.LENGTH_SHORT).show();
+                } else if(TextUtils.isEmpty(str_password)){
+                    Toast.makeText(RegisterActivity.this, "Please enter Password", Toast.LENGTH_SHORT).show();
+                } else if(str_password.length()<6){
+                    Toast.makeText(RegisterActivity.this, "Password must contain atleast 6 characters ", Toast.LENGTH_SHORT).show();
+                } else{
+                    register(str_username,str_fullname,str_email,str_password);
+                }
+
+            }
+        });
+
+    }
+
+    private void register (String username,String fullname,String email,String password){
+        auth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            FirebaseUser firebaseUser = auth.getCurrentUser();
+                            String userid = firebaseUser.getUid();
+
+                            reference = FirebaseDatabase.getInstance().getReference().child("Users").child(userid);
+
+                            HashMap<String,Object> hashmap = new HashMap<>();
+                            hashmap.put("id",userid);
+                            hashmap.put("username",username.toLowerCase());
+                            hashmap.put("fullname",fullname);
+                            hashmap.put("bio",userid);
+                            //hashmap.put("imageurl",userid);
+
+                            reference.setValue(hashmap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        progressDialog.dismiss();
+                                        Intent intent = new Intent(RegisterActivity.this,MainActivity.class);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        startActivity(intent);
+                                    }
+                                }
+                            });
+                        } else{
+                            progressDialog.dismiss();
+                            Toast.makeText(RegisterActivity.this, "You can't register with this emial and password", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+}
---
+++ app/src/main/java/com/example/instagram/StartActivity.java
@@ -0,0 +1,60 @@
+package com.example.instagram;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.ktx.Firebase;
+
+public class StartActivity extends AppCompatActivity {
+
+    private Button login,register;
+
+    FirebaseUser firebaseUser;
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        //redirect if user in not null
+        if(firebaseUser != null){
+            startActivity(new Intent(StartActivity.this,MainActivity.class));
+            finish();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_start);
+
+        login = findViewById(R.id.login);
+        register = findViewById(R.id.register);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(StartActivity.this,LoginActivity.class));
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(StartActivity.this,RegisterActivity.class));
+            }
+        });
+    }
+}
