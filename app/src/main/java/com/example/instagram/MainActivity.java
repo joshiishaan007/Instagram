6b7cb2151b50ab05897ea54920bbc5a0a3b5fbbd
joshiishaan007 <joshiishaan007@gmail.com>
2024-09-14 11:55:43 +0530

    bottom navigation and fragments

--- app/src/main/java/com/example/instagram/MainActivity.java
+++ app/src/main/java/com/example/instagram/MainActivity.java
@@ -1,24 +1,64 @@
 package com.example.instagram;
 
+import android.annotation.SuppressLint;
+import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import fragment.homeFragment;
+import fragment.notificationFragment;
+import fragment.searchFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    Fragment selectedFragment = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                    int itemId = item.getItemId();
+
+                    if(itemId == R.id.nav_home){
+                        selectedFragment = new homeFragment();
+                    }else if (itemId == R.id.nav_search){
+                        selectedFragment = new searchFragment();
+                    } else if (itemId == R.id.nav_add) {
+                        selectedFragment = null;
+                        startActivity(new Intent(MainActivity.this,PostActivity.class));
+
+                    } else if (itemId == R.id.nav_reels) {
+                        selectedFragment = new notificationFragment();
+
+                    }else if (itemId == R.id.nav_profile) {
+                        selectedFragment = new homeFragment();
+
+                    }
+
+                    return true;
+                }
+            };
 }
